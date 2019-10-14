f9397bb606d9e09f7788cdd9db29d8a49f550260
TTwithTT <puttyo76@gmai.com>
2019-10-14 08:48:52 +0000

    edit orders, order_items controller

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,13 @@
 class OrderItemsController < ApplicationController
+  def create
+    @order_item = OderItem.new(item_params)
+  end
+
+  def index
+    @order_items = OrderItem.all
+  end
+
+  private
+  def order_item_params
+    params.require(:order_item).permit(:order_price, :order_amount)
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,14 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.page(params[:page]).reverse_order
+  end
+  def create
+    @order = Order.new(order_params)
+    @order.user_id = current_user.id
+    @order.save
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :total_price, :shipping_status, :shipping_name, :shipping_address, :shipping_price)
 end
